967f51f059be0eaf10131f8dc4738c6af322b287
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-07-25 02:27:10 +0300

    some update

--- sql-seek-bak-pass-3.sql
+++ sql-seek-bak-pass-3.sql
@@ -3,3 +3,25 @@ SELECT casewhen(length(ert_chut)<=2,1,0) ert_chut_notchut, x.* FROM (
 SELECT REGEXP_REPLACE(REGEXP_REPLACE(s.COMMENT,'Ертапенем +',''),' +чутл.','') ert_chut, 
 s.COMMENT cmt1, s.* FROM surgery17_1 s where COMMENT like 'Ерта%'
 ) x
+
+-- Гриби
+SELECT * FROM (
+SELECT ol.* FROM surgery17_1 s, ORGLIST ol, (SELECT  MIN(id) mid FROM ORGLIST ol GROUP BY org) x 
+WHERE x.mid=ol.id and s.organism=ol.org
+) x WHERE gram='f'
+
+-- all normal probe
+SELECT * FROM (
+SELECT * FROM surgery17_1 s, ORGLIST ol, (SELECT  min(id) mid FROM ORGLIST ol GROUP BY org) x 
+WHERE x.mid=ol.id and s.organism=ol.org
+) x 
+where not(gram='f' or COMMENT_aberration)
+
+-- all probe
+SELECT ol.* FROM surgery17_1 s, ORGLIST ol, (SELECT  min(id) mid FROM ORGLIST ol GROUP BY org) x 
+WHERE x.mid=ol.id and s.organism=ol.org
+
+-- unique orglist
+SELECT * FROM ORGLIST ol, (SELECT  min(id) mid FROM ORGLIST ol GROUP BY org) x WHERE x.mid=ol.id
+
+
